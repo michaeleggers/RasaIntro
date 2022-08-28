c836559ede3cccf3f6143a644ba90bbc9762838c
Michael Mueller <michael.mueller@hm.edu>
2022-08-28 17:21:00 +0200

    feat: try custom action determine next action

--- project-neo-test-1/actions/actions.py
+++ project-neo-test-1/actions/actions.py
@@ -3,25 +3,21 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
-
-
+#
+#
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "custom_find_course_path"
+
+    def run(self, dispatcher, tracker, domain):
+            dispatcher.utter_message(response="form_study_organization")
+            return []
