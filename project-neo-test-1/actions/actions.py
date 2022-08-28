0a854ea3ef71acb1d22d03ff71a48420e35ca017
Michael Mueller <michael.mueller@hm.edu>
2022-08-28 18:03:52 +0200

    feat: custom action with triggering form works

--- project-neo-test-1/actions/actions.py
+++ project-neo-test-1/actions/actions.py
@@ -11,7 +11,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
+from rasa_sdk.events import FollowupAction, EventType
 
 class ActionHelloWorld(Action):
 
@@ -19,5 +19,7 @@ class ActionHelloWorld(Action):
         return "custom_find_course_path"
 
     def run(self, dispatcher, tracker, domain):
-            dispatcher.utter_message(response="form_study_organization")
-            return []
+        print("foo")
+        # dispatcher.utter_message(text="utter_ask_study_organization")
+        # return []
+        return [FollowupAction(name="form_study_organization")]
